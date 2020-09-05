41047bb68527526aa2ce8474ec0033e2f83f71b5
Arkadipta De <46627785+Arko98@users.noreply.github.com>
2020-09-05 15:59:35 +0530

    Smallest Element in Increasing Sequence rotated unknown times

---
+++ Find_Minimum_Rotated.py
@@ -0,0 +1,22 @@
+def find_rotated_min(array,left,right):
+  '''
+  Function to find minimum element in an increasing sequence 
+  rotated unknown numberof times in O(logn) time
+  '''
+  # When search is exhausted, condition not met, array is not rotated then 1st element is smallest
+  if right<left:
+    return array[0]
+  # Only one element is left to check then that element is smallest
+  if left == right:
+    return array[left]
+  mid = (left+right)//2
+  if (mid>left and array[mid]<array[mid-1]):
+    return array[mid]
+  if (mid<right and array[mid+1]<array[mid]):
+    return array[mid+1]
+  if (array[right]>array[mid]):
+    return find_rotated_min(array,left,mid-1)
+  return find_rotated_min(array,mid+1,right)
+
+A = [12,23,33,34,56,79,5,8,9,10]
+print(find_rotated_min(A,0,len(A)-1))
