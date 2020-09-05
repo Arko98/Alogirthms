8e3f8a449fe3dc64e3558956d6b34d1aaa8906ff
Arkadipta De <46627785+Arko98@users.noreply.github.com>
2020-09-05 15:24:17 +0530

    O(LogN) method to for min item in Special Sequence
    Divide and Conquer Method to find smallest element in a sequence that is decreasing and then increasing
    Example A = [50,34,21,12,6,3,5,7,9,24]

---
+++ Find_Min.py
@@ -0,0 +1,18 @@
+def find_min(array,left,right):
+  '''
+  Function to find minimum element in a decreasing and 
+  then increasing sequence in O(logn) time
+  '''
+  if (left<=right):
+    mid = (left + right)//2
+    if (array[mid]<array[mid+1] and array[mid]<array[mid-1]):
+      print(array[mid])
+      return
+    elif (array[mid]>array[mid+1]):
+      left = mid+1
+      find_min(array,left,right)
+    right = mid-1
+    find_min(array,left,right)
+
+A = [43,41,33,27,13,8,6,2,5,7,21,55]
+find_min(A,0,len(A)-1)
