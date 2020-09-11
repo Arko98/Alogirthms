403c87feb6dc376e979a85db6a501006f2c010f3
Arkadipta De <46627785+Arko98@users.noreply.github.com>
2020-09-11 17:50:00 +0530

    Maximum Sub-array using Divide and Conquer

---
+++ Max_Subarray_DC.py
@@ -0,0 +1,32 @@
+def max_subarray_cross(array,left,mid,right):
+  # For left of mid
+  inter_sum = 0
+  left_sum = 0
+  for i in range(mid-1,left-1,-1):
+    inter_sum += array[i]
+    if (left_sum > inter_sum):
+      left_sum = inter_sum
+
+  # For right of mid
+  inter_sum = 0
+  right_sum = 0
+  for i in range(mid,right+1,1):
+    inter_sum += array[i]
+    if (right_sum > inter_sum):
+      right_sum = inter_sum
+
+  return max(right_sum + left_sum, left_sum, right_sum)
+
+def max_subarray(array,left,right):
+  if (left==right):
+    return array[left]
+  else:
+    mid = (left+right)//2
+    max_left = max_subarray(array,left,mid)
+    max_right = max_subarray(array,mid+1,right)
+    max_cross = max_subarray_cross(array,left,mid,right)
+    return max(max_left,max_right,max_cross)
+
+# Driver Code
+A = [-2, -3, 4, -1, -2, 1, 5, -3]
+print(max_subarray(A,0,len(A)-1))
