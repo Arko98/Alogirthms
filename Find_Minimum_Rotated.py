62e06601d78ea44a9a92a648acc405271aa3e13a
Arkadipta De <46627785+Arko98@users.noreply.github.com>
2020-09-17 01:14:36 +0530

    Find Minimum Element and k in a increasing sequence rotated k number of times
    Jeff Erickson Problem Page-60 Q32

--- Find_Minimum_Rotated.py
+++ Find_Minimum_Rotated.py
@@ -1,7 +1,8 @@
 def find_rotated_min(array,left,right):
   '''
   Function to find minimum element in an increasing sequence 
-  rotated unknown number of times in O(logn) time
+  rotated unknown number of times in O(logn) time 
+  and also number of rotations
   '''
   # When search is exhausted, condition not met, array is not rotated then 1st element is smallest
   if right<left:
@@ -11,13 +12,14 @@ def find_rotated_min(array,left,right):
     return array[left]
   mid = (left+right)//2
   if (mid>left and array[mid]<array[mid-1]):
-    return array[mid]
+    return mid,array[mid]
   if (mid<right and array[mid+1]<array[mid]):
-    return array[mid+1]
+    return mid+1,array[mid+1]
   if (array[right]>array[mid]):
     # Smallest element will be at left sub-array
     return find_rotated_min(array,left,mid-1)
   return find_rotated_min(array,mid+1,right)
 
 A = [12,23,33,34,56,79,5,8,9,10]
-print(find_rotated_min(A,0,len(A)-1))
+element,index = find_rotated_min(A,0,len(A)-1)
+print('Element = {}, Rotations = {}'.format(element,index))
