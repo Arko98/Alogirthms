f3cdab47d2fcdea55464956326a42c8f76712808
Arkadipta De <de.arkadipta05@gmail.com>
2020-09-28 00:15:19 +0530

    Fast Inversion Count in Array using Merge Sort in O(nlogn)

---
+++ Count Inversions.cpp
@@ -0,0 +1,66 @@
+#include<stdio.h>
+#include<conio.h>
+
+int count = 0;
+
+void merge(int array[], int begin,int mid,int end)
+	{   
+		int i=begin,j=mid+1,index=begin,k=0,temp[100];
+		while (i<=mid && j<=end)
+			{
+				if (array[i]<=array[j])
+					{
+						temp[index]=array[i];
+						i++;
+					}
+				else
+					{
+					    count += (mid-i+1); 
+						temp[index]=array[j];
+						j++;
+					}
+				index++;
+			}
+
+			while(j<=end)
+				{
+					temp[index]=array[j];
+					j++;
+					index++;
+				}
+			while(i<=mid)
+				{
+					temp[index]=array[i];
+					i++;
+					index++;
+			    }
+		for (k=begin;k<=end;k++)
+			{
+				array[k]=temp[k];
+			}
+	}
+
+void mergesort(int array[], int begin, int end)
+	{    
+		if (begin<end)
+			{
+				int mid = (begin+end)/2;
+				mergesort(array,begin,mid);
+			    mergesort(array,mid+1,end);
+				merge(array,begin,mid,end);
+			}
+	}
+	
+int main()
+	{
+		int i,n;
+		scanf("%d",&n);
+		int a[n];
+		for (i=0;i<n;i++)
+			scanf("%d",&a[i]);
+		mergesort(a,0,n-1);
+		for (i=0;i<n;i++)
+			printf("%d ",a[i]);
+		printf("\nInversions: %d",count);
+		return 0;
+	}	
